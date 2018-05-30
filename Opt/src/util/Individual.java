63c6b913e782a96b97fe72558d98546433426922
David_Puerta <dpuerta@bvc.com.co>
2018-05-30 11:35:43 -0500

    new

---
+++ Opt/src/util/Individual.java
@@ -0,0 +1,14 @@
+package util;
+
+public class Individual {
+	private double[] solution;
+
+	public double[] getSolution() {
+		return solution;
+	}
+
+	public void setSolution(double[] solution) {
+		this.solution = solution;
+	}
+	
+}
