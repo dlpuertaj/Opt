7c21138f11bdbd70bfdb23a570f113469980e714
David Leonardo <dlpuertaj@unal.edu.co>
2018-08-04 18:08:17 -0500

    Opt

--- Opt/src/problems/Rastrigin.java
+++ Opt/src/problems/Rastrigin.java
@@ -1,22 +1,37 @@
 package problems;
 
-public class Rastrigin extends Problem{
+public class Rastrigin {
+	private int n;
+	private int A;
 
-	private final int A = 10;
-
-	
-	public Rastrigin(double[] bounds, int dim,boolean opt) {
-		super(bounds,dim,opt);
+	public Rastrigin (int n, int A) {
+		this.n = n;
+		this.A = A;
 	}
 	
-	@Override
-	public double calc(double[] x) {
-		double s = 0;
-		
-		for (double xi : x) {
-			s = (xi*xi) - A*Math.cos(2*Math.PI*xi);
+	public double calc(double[] X) {
+		double sum = 0;
+		for(int i = 0 ; i < n ; i++) {
+			sum += Math.pow(X[i], 2) - A*Math.cos(2*Math.PI*X[i]);
 		}
-		return A*super.getDim() + s;
+		return (A*n) + sum;				
 	}
+
+	public int getN() {
+		return n;
+	}
+
+	public void setN(int n) {
+		this.n = n;
+	}
+
+	public int getA() {
+		return A;
+	}
+
+	public void setA(int a) {
+		A = a;
+	}
+	
 	
 }
